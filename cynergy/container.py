b7e42f8eed380cd3ba7678c4ea35120501550126
omersaraf <omersaraf.develop@gmail.com>
2019-10-22 22:53:42 +0300

    Support for Python3.7

--- cynergy/container.py
+++ cynergy/container.py
@@ -1,5 +1,6 @@
 import inspect
-import typing
+from typing import TypeVar, Optional, Type, List, Dict
+
 from warnings import warn
 
 from cynergy.attributes import LifeCycle
@@ -9,11 +10,11 @@ from cynergy.config import ConfigProvider, Plain, Config, ServiceByName
 from cynergy import attributes
 from cynergy.errors.ContainerException import ConfigProviderRequiredException
 
-T = typing.TypeVar('T')
+T = TypeVar('T')
 
 
 class IocContainer(object):
-    def __init__(self, config_provider: typing.Optional[ConfigProvider]):
+    def __init__(self, config_provider: Optional[ConfigProvider]):
         self.__multiple_class_mapping = {}
         self.__instances = {}
         self.__class_mapping = {}
@@ -47,11 +48,10 @@ class IocContainer(object):
         raise NotImplementedError(
             'Argument type "{}" is not supported (did you pass argument type?)'.format(type(argument)))
 
-    def __create_instance_for_single(self, class_to_init: typing.Type, original: typing.Type):
+    def __create_instance_for_single(self, class_to_init: Type, original: Type):
         arguments_mapping = {}
         if hasattr(class_to_init, attributes.IOC_ARGUMENTS_NAME):
             arguments_mapping = getattr(class_to_init, attributes.IOC_ARGUMENTS_NAME)
-
         try:
             arguments = inspect.signature(class_to_init)
         except Exception:
@@ -79,45 +79,37 @@ class IocContainer(object):
             initiated_arguments[argument.name] = initiated_argument
         self.__set_instance(original, class_to_init(**initiated_arguments))
 
-    def __set_instance(self, cls: typing.Type, instance):
-        self.__instances[self.__get_class_name(cls)] = instance
+    def __set_instance(self, cls: Type, instance):
+        self.__instances[self._get_class_name(cls)] = instance
 
-    def __get_instance(self, cls: typing.Type):
-        return self.__instances[self.__get_class_name(cls)]
+    def __get_instance(self, cls: Type):
+        return self.__instances[self._get_class_name(cls)]
 
-    def __create_instance_for_list(self, cls: typing.Type, original: typing.Type):
-        cls = cls.__args__[0]
-        classes_to_init = cls if cls.__name__ not in self.__class_mapping else self.__class_mapping[cls.__name__]
-        if type(classes_to_init) is not list:
-            self.__set_instance(original, [self.get(classes_to_init)])
+    def __create_instance_for_list_new(self, original, classes: List[Type]):
+        self.__set_instance(original, [self.get(cls) for cls in classes])
 
-        else:
-            result = []
-            for class_to_init in classes_to_init:
-                result.append(self.get(class_to_init))
-            self.__set_instance(original, result)
-
-    def __create_instance(self, cls: typing.Type):
-        class_to_init = cls if cls.__name__ not in self.__class_mapping else self.__class_mapping[cls.__name__]
-        if class_to_init.__name__ is typing.List.__name__:
-            self.__create_instance_for_list(class_to_init, cls)
+    def __create_instance(self, cls: Type):
+        cls_name = self._get_class_name(cls)
+        class_to_init = cls if cls_name not in self.__class_mapping else self.__class_mapping[cls_name]
+        if isinstance(class_to_init, list):
+            self.__create_instance_for_list_new(cls, class_to_init)
         else:
             self.__create_instance_for_single(class_to_init, cls)
         return self.__get_instance(cls)
 
     def register_instance(self, cls, instance):
-        self.__register_instance_by_name(cls.__name__, instance)
+        self.__register_instance_by_name(self._get_class_name(cls), instance)
 
-    def register_class(self, cls, assign_to: typing.Type):
-        self.__class_mapping[cls.__name__] = assign_to
+    def register_class(self, cls, assign_to: Type):
+        self.__class_mapping[self._get_class_name(cls)] = assign_to
 
-    def register_many(self, cls, new_classes: typing.List[typing.Type]):
-        self.__class_mapping[cls.__name__] = new_classes
+    def register_many(self, cls, new_classes: List[Type]):
+        self.__class_mapping[self._get_class_name(List[cls])] = new_classes
 
     @staticmethod
-    def __get_class_name(cls: typing.Type):
-        if type(cls) == type(typing.List):
-            return cls.__args__[0].__name__
+    def _get_class_name(cls: Type):
+        if type(cls) == type(List):
+            return 'List[{}]'.format(cls.__args__[0].__name__)
         return cls.__name__
 
     def __register_instance_by_name(self, name, instance):
@@ -132,14 +124,14 @@ class IocContainer(object):
             KeyError('The service "{}" is not registered'.format(key))
         return self.__instances[key]
 
-    def get(self, cls: typing.Type[T], life_cycle=LifeCycle.SINGLETON) -> T:
+    def get(self, cls: Type[T], life_cycle=LifeCycle.SINGLETON) -> T:
         if not life_cycle:
             life_cycle = attributes.LifeCycle.SINGLETON
             if hasattr(cls, attributes.LIFECYCLE_ARGUMENT_NAME):
                 life_cycle = getattr(cls, attributes.LIFECYCLE_ARGUMENT_NAME)
 
         if life_cycle == attributes.LifeCycle.SINGLETON:
-            if self.__get_class_name(cls) not in self.__instances:
+            if self._get_class_name(cls) not in self.__instances:
                 self.__create_instance(cls)
             return self.__get_instance(cls)
         elif life_cycle == attributes.LifeCycle.MULTI_INSTANCE:
@@ -150,7 +142,7 @@ class IocContainer(object):
     def clear_all(self):
         self.__instances = {}
 
-    def register(self, cls: typing.Type, class_or_instance):
+    def register(self, cls: Type, class_or_instance):
         if isinstance(class_or_instance, list):
             self.register_many(cls, class_or_instance)
         elif inspect.isclass(class_or_instance):
@@ -178,8 +170,8 @@ def get_config(key: str):
     return __get_instance().get_config(key)
 
 
-def initialize(config_provider: typing.Optional[ConfigProvider] = None,
-               class_mapping: typing.Dict[typing.Type, typing.Type] = None):
+def initialize(config_provider: Optional[ConfigProvider] = None,
+               class_mapping: Dict[Type, Type] = None):
     global __instance
     if __instance is not None:
         warn("Container already initialized. If you need multiple instances consider not use the container statically",
@@ -201,7 +193,7 @@ def register_instance_by_name(name, instance):
     return __get_instance().register_instance_by_name(name, instance)
 
 
-def get(cls: typing.Type[T], life_cycle=LifeCycle.SINGLETON) -> T:
+def get(cls: Type[T], life_cycle=LifeCycle.SINGLETON) -> T:
     return __get_instance().get(cls, life_cycle)
 
 
@@ -228,5 +220,5 @@ def get_by_name(name):
     return __get_instance().get_by_name(name)
 
 
-def register_many(cls: typing.Type, types: typing.List[typing.Type]):
+def register_many(cls: Type, types: List[Type]):
     return __get_instance().register_many(cls, types)
